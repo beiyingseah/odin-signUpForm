62982a4a66373b5cb32d108571db9dffb303bfbe
Seah Bei Ying <beiyingseah@gmail.com>
2022-07-08 15:08:40 +0300

    Minor formatting edits

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-const pass=document.querySelectorAll('input[type="password"]');
-pass.forEach(pass=>pass.addEventListener('blur',checkPasswords));
+// Application of attribute selector
+const password = document.querySelectorAll('input[type="password"]');
+password.forEach(password => password.addEventListener('blur',checkPasswords));
 
 function checkPasswords(){
-    if(pass[0].value==pass[1].value){
-        pass.forEach(pass=>pass.classList.remove('error'));
-        pass[0].parentNode.classList.remove('error');
+    if(password[0].value==password[1].value){
+        password.forEach(password=>password.classList.remove('error'));
+        password[0].parentNode.classList.remove('error');
     }
     else{
-        pass.forEach(pass=>pass.classList.add('error'));
-        pass[0].parentNode.classList.add('error')
+        password.forEach(password=>password.classList.add('error'));
+        password[0].parentNode.classList.add('error')
     }
     
 }
